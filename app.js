50e8e6c06fdc653707b698a4e2d274440053988c
Humberto García <90355450+bertochgg@users.noreply.github.com>
2022-06-15 18:25:40 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+let encryptBtn = document.getElementById('encrypt-btn');
+let eInput = document.getElementById('encryptedInput-1');
+let pInput = document.getElementById('plainInput-1');
+let inputs = [eInput, pInput];
+
+inputs.forEach(input =>{
+    input.oninput = () =>{
+        input.value = input.value.toUpperCase();
+    }
+})
+
+function encrypt(){
+    let pInput = document.getElementById('plainInput-1').value;
+    let solved = '';
+    let shiftInput = parseInt(document.getElementById('shiftInput').value)
+
+    for(var i = 0; i < pInput.length; i++){
+        let ascii_num = pInput[i].charCodeAt();
+        let sum = ascii_num + shiftInput;
+        sum >= 65 && sum <= 90 ? solved += String.fromCharCode(sum) : sum > 90 ? solved += String.fromCharCode(65 + (sum & 91)) : solved += pInput[i];
+    }
+    eInput.value = solved;
+}
+
+encryptBtn.addEventListener('click', encrypt)
